d6430835339d640bdda09bf248b519638f85d370
Brandon Berhent <bbedward@gmail.com>
2018-04-10 13:51:09 +0000

    Triple random giveaway winner chooser algorithm - Basically first we select all contestants ordered by sqlite.RANDOM() (queried_list) - Then we use a Fisher-Yates shuffle on the queries_list of user IDs (shuffled_list) - Then we use the Mersenne Twister PRNG to generate a random number [0, len(shuffled_list)) (x) - Then we grab the contestant at index x in shuffled_list
    Mostly funny overkill to squish criticisms of the algorithm that picks giveaway winners

--- db.py
+++ db.py
@@ -319,8 +319,13 @@ def get_statsbanned():
 
 # Returns winning user
 def finish_giveaway():
-	contestants = Contestant.select().where(Contestant.banned == False).order_by(fn.Random())
-	winner = shuffle(contestants)[randint(0, contestants.count() - 1)]
+	contestants = Contestant.select(Contestant.user_id).where(Contestant.banned == False).order_by(fn.Random())
+	contestant_ids = []
+	for c in contestants:
+		contestant_ids.append(c.user_id)
+	shuffle(contestant_ids)
+	offset = randint(0, len(contestant_ids) - 1)
+	winner = get_user_by_id(contestant_ids[offset])
 	Contestant.delete().execute()
 	giveaway = Giveaway.get(active=True)
 	giveaway.active=False
